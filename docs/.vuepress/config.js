3632cda78ca9f3782ad9d1e092e4b309cf903067
Adam <shiguangle@styd.cn>
2021-03-17 17:50:46 +0800

    feat: upload

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  logo: '/public/jerry.jpeg',
+  title: '每周一砖',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebarDepth: 3,
+    displayAllHeaders: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Guide',
+        link: '/guide/'
+      }
+    ],
+    sidebar: [
+      '/process-thread/',
+      ['/js/eventLoop.md', 'js']
+    ]
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}
---
+++ docs/guide/README.md
@@ -0,0 +1,3 @@
+# 指南
+
+本网站由vuePress构建，具体可参考[https://vuepress.vuejs.org/zh/guide/](https://vuepress.vuejs.org/zh/guide/)
